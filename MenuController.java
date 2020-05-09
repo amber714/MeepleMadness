02f630c22f3ed3a6b26f861088ef2681ecd86191
Otimeyin Efejuku <61487389+Oti543@users.noreply.github.com>
2020-05-08 23:17:25 -0500

    Add files via upload

--- MenuController.java
+++ MenuController.java
@@ -11,7 +11,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-
+import application.model.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import javafx.scene.layout.StackPane;
 
 /**
  * 
@@ -26,7 +34,7 @@ public class MenuController{
 	
 	@FXML
 	private Label inProgress;
-	*/
+*/
 	
 	@FXML
 	private Button backButton;
@@ -43,17 +51,19 @@ public class MenuController{
 	
 	public void handle(ActionEvent event) {
 		
+		
+		
 		try {
-			
+
 			if(((Button) event.getSource()).getText().equals(backButton.getText())) { 
 
 				FXMLLoader loader = new FXMLLoader();
 				loader.setLocation(getClass().getResource("/application/view/Home.fxml"));
 				Parent root = loader.load();
 				Main.s.setScene(new Scene(root, 800, 800));
-				Main.s.show();
+				Main.s.show(); 
 			
-		}
+		} 
 			else if((((Button) event.getSource()).getText().equals(addButton.getText()))) { 
 
 				FXMLLoader loader = new FXMLLoader();
@@ -70,4 +80,35 @@ public class MenuController{
 	
 	
 }
+	public void display(){
+		try {
+			completedList = new ListView<String>();
+			inProgressList = new ListView<String>();
+			
+			
+			File cfile = new File("completeData.csv");
+			File pfile = new File("wipData.csv");
+			String charset = "US-ASCII";
+			BufferedReader cReader = new BufferedReader(new InputStreamReader(new FileInputStream(cfile), charset));
+			for(String line; (line = cReader.readLine()) !=null;){
+				line = line.replace(",", " ");
+				completedList.getItems().add(line);
+			}
+			
+			BufferedReader pReader = new BufferedReader(new InputStreamReader(new FileInputStream(pfile), charset));
+			for(String line; (line = pReader.readLine()) !=null;){
+				line = line.replace(",", " - ");
+				inProgressList.getItems().add(line);
+			}
+			pReader.close();
+			cReader.close();
+			
+			StackPane stackPane = new StackPane(completedList);
+			StackPane stackPaneProgress = new StackPane(inProgressList);
+			
+			
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 }
